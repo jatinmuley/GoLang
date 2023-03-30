9b2ac7e9a115fd494f1f529c543913939328dc58
jatinmuley <40910698+jatinmuley@users.noreply.github.com>
2023-03-31 00:12:00 +1100

    slices updat

---
+++ day8slices/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Slices ")
+	var fruitlist = []string{"mango","banana","apple"}
+	fmt.Println("fruitlist is ",fruitlist)
+fruitlist = append(fruitlist, "peach","kiwi")
+fmt.Println("fruitlist is ",fruitlist)
+
+// slicing the slice 
+fruitlist = append(fruitlist[1:])
+fmt.Println(fruitlist)
+
+fruitlist = append(fruitlist[1:3])
+fmt.Println(fruitlist)
+
+
+highscore := make([]int,4 )
+highscore[0] = 232
+highscore[1] = 134
+highscore[2] = 534
+highscore[3] = 634
+
+fmt.Println("highscore is ",highscore)
+highscore = append(highscore , 232,333,3334)
+fmt.Println("Highsocre updates",highscore)
+sort.Ints(highscore)
+fmt.Println(highscore)
+ fmt.Println(sort.IntsAreSorted(highscore))
+
+}
